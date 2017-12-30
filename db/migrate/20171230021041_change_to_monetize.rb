3acd982866b326f6415254b127119567f64a3c38
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-12-30 11:32:12 -0500

    db_fix_monetize1

--- db/migrate/20171230021041_change_to_monetize.rb
+++ db/migrate/20171230021041_change_to_monetize.rb
@@ -2,8 +2,6 @@ class ChangeToMonetize < ActiveRecord::Migration[5.0]
   def change
     if Rails.env.development?
       remove_money :products, :price
-    else
-      remove_monetize :products, :price
     end
     add_monetize :products, :price
   end
