08bd4238cc2264a7132b5111a8837eb99f788b00
ethanwilkins <ethanforrestwilkins@gmail.com>
2017-12-30 10:23:55 -0500

    money_rails1

--- db/migrate/20171230021938_change_price_to_integer.rb
+++ db/migrate/20171230021938_change_price_to_integer.rb
@@ -1,6 +1,4 @@
 class ChangePriceToInteger < ActiveRecord::Migration[5.0]
   def change
-    remove_money :products, :price
-    add_column :products, :price, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230021041) do
+ActiveRecord::Schema.define(version: 20171230021938) do
 
   create_table "bot_tasks", force: :cascade do |t|
     t.integer  "user_id"
