5f07e71c798b5bc70af3c6e3bd8f8546bbd72352
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-01-04 00:08:30 -0500

    fix?11

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,20 @@ class Setting < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name
   
+  def self.settings user
+    if user
+      setting = lambda { |name| user.settings.find_by_name name }
+      settings = {}; self.names.each do |category, names|
+        for name in names
+          settings[name.to_sym] = setting.call(name).send(category)
+        end
+      end
+      return settings
+    else
+      return {}
+    end
+  end
+  
   # run when self.names is changed or added to
   def self.initialize_all_settings
     for user in User.all
