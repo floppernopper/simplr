e818c2be8ea663ce35d19cd3e17c8bb557535eb4
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-01-03 23:35:24 -0500

    fix?1

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -11,9 +11,7 @@ class Setting < ActiveRecord::Base
   
   def self.names
     { on: ['bg_fader_on', 'post_bg_fader_on', 'post_txt_fader_on',
-      'profile_card_bg_fader_on', 'profile_bg_fader_on',
-      'chrono_feed_on'],
-    
+      'profile_card_bg_fader_on', 'profile_bg_fader_on', 'chrono_feed_on'],
       state: ['bg_color', 'post_bg_color', 'post_txt_color', 'profile_card_bg_color', 'profile_bg_color'] }
   end
 end
