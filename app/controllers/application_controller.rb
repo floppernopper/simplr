0a7742e8b4aa2445a689e19aa398b9c37c1e0e12
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-02-04 19:01:07 -0500

    is_gatekeeper?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :anon_token, :current_user, :current_identity, :mobile?, :browser, :get_location,
     :page_size, :paginate, :reset_page, :char_codes, :char_bits, :settings, :dev?, :anrcho?, :invited?,
     :seen?, :seent, :get_site_title, :record_last_visit, :probably_human, :god?, :goddess?, :currently_kristin?,
-    :forrest_only_club?, :invited_to_forrest_only_club?, :in_dev?, :gatekeeper?, :page_turning,
+    :forrest_only_club?, :invited_to_forrest_only_club?, :in_dev?, :is_gatekeeper?, :page_turning,
     :testing_score?, :unique_element_token
   
   include SimpleCaptcha::ControllerHelpers
@@ -236,7 +236,7 @@ class ApplicationController < ActionController::Base
       or current_user or User.all.size.zero? or cookies[:zen].present?
   end
   
-  def gatekeeper?
+  def is_gatekeeper?
     current_user and current_user.gatekeeper and User.where(gatekeeper: true).last.eql? current_user
   end
   
