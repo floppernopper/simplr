665bb219750f7b9adfa8000118135d2207994620
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-03-09 02:41:52 -0500

    like_like_fixed?1

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -37,7 +37,7 @@ module LikesHelper
     else
       post.likes.find_by_anon_token anon_token
     end
-    #like = nil
+    #like = nil # why'd I ever put this here??
     if like and like.likes.present?
       like = if note.sender_id
         like.likes.find_by_user_id note.sender_id
