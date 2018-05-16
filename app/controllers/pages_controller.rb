a46415dc56fa16871dd97822ab22542f9bdef833
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-05-16 14:48:14 -0400

    in_dev_anrcho_and_buffer_wheel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   before_action :set_human
-  
+
   def resume
   end
-  
+
   def more
     page_turning relevant_items
     build_feed_data
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
 
   def scroll_to_top
   end
-  
+
   def toggle_menu
     # if nav menu is already open and was opened in the last 10 seconds
     if session[:nav_menu_shown].present? and session[:nav_menu_shown_at].to_datetime > 5.second.ago
@@ -23,13 +23,13 @@ class PagesController < ApplicationController
       session[:nav_menu_shown_at] = DateTime.current
     end
   end
-  
+
   private
 
   def set_human
     cookies.permanent[:probably_human] = true unless current_user
   end
-  
+
   def build_feed_data
     @all_items = relevant_items
     # applies to all item types
@@ -39,12 +39,12 @@ class PagesController < ApplicationController
     # records being seen
     @items.each {|item| seent item}
   end
-  
+
   def relevant_items
     # for anarcho
     if params[:proposals]
       @proposals = true
-      return Proposal.globals.sort_by { |proposal| proposal.score }
+      return (in_dev? ? Proposal.all : Proposal.globals).sort_by { |proposal| proposal.score }
     # for social_maya
     elsif params[:posts]
       @home_shown = true
