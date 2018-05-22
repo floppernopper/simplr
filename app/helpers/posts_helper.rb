badc332205295b50aee20afb8a5accfca98cb830
ethanwilkins <ethanforrestwilkins@gmail.com>
2018-05-22 12:56:59 -0400

    made_a_motion_in_anrcho_portal_link_label

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,8 @@
 module PostsHelper
+  def show_card? post
+    #if post.group.nil? or group_auth post.group or group_member_auth post.group or (visible_to_anons? post.group and not post.group.hidden) or own_item? post) and not ((post.hidden or (post.user and post.user.hidden)) and not own_item? post)
+  end
+
   def original_post_pictures post
     if post.original_id
       original = Post.find_by_id post.original_id
